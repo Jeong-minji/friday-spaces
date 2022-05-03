424b51afa4ffac67c4730418bc59afa243bf061b
Genie <jeonbg@kakao.com>
2022-05-04 00:55:22 +0900

    Change : Next-Auth User Props 1. Add UserTypes.d.ts 2. Change Next-Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,19 +1,45 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { UserTypes } from 'types/User'
 
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'repo read:user user:email',
+        },
+      },
     }),
   ],
   debug: process.env.NODE_ENV === 'development',
   secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
   jwt: {
     secret: process.env.JWT_SECRET,
   },
   callbacks: {
+    async jwt({ token, user, profile }) {
+      if (user) {
+        token.user = {
+          ...user,
+          ...profile,
+        }
+      }
+      return token
+    },
+    async session({ session, token }) {
+      const users = token.user as UserTypes
+      session = {
+        ...session,
+        user: users,
+      }
+      return session
+    },
     async redirect() {
       return '/'
     },
