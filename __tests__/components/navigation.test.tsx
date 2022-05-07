f7003c50ed7eb4ea0eb7a7c4528477c389e63968
Genie <jeonbg@kakao.com>
2022-05-07 21:48:55 +0900

    Add : User Component 1. Change Navigation Test 2. Add User Component

--- __tests__/components/navigation.test.tsx
+++ __tests__/components/navigation.test.tsx
@@ -2,15 +2,31 @@ import { Navigation } from 'components'
 import { ThemeProvider } from '@emotion/react'
 import theme from 'styles/theme'
 import { render, screen } from '@testing-library/react'
+import { DefaultUser, Session } from 'next-auth'
+import { useSession } from 'next-auth/react'
+import { UserTypes } from 'types/User'
+
+jest.mock('next-auth/react')
 
 describe('Navigation', () => {
   it('Should Navigation Render Success', async () => {
+    const mockSession: Session = {
+      expires: new Date(Date.now() + 2 * 86400).toISOString(),
+      user: {
+        id: 85790271,
+      } as DefaultUser & UserTypes,
+    }
+    ;(useSession as jest.Mock).mockReturnValueOnce([
+      mockSession,
+      'authenticated',
+    ])
+
     render(
       <ThemeProvider theme={theme}>
         <Navigation />
       </ThemeProvider>
     )
-    const navigationText = screen.getByText(/Navigation/i)
+    const navigationText = screen.getByTestId(/navigation/i)
     expect(navigationText).toBeInTheDocument()
   })
 })
--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,7 +1,14 @@
+import User from 'components/common/User/User'
+import { useSession } from 'next-auth/react'
 import { NavigationStyle } from './Style'
 
 const Navigation = () => {
-  return <NavigationStyle>Navigation</NavigationStyle>
+  const { data } = useSession()
+  return (
+    <NavigationStyle data-testid="navigation">
+      <User username={data?.user?.name} thumbnail={data?.user?.avatar_url} />
+    </NavigationStyle>
+  )
 }
 
 export default Navigation
